72d1216c9261ff4b8b481829c902a1b164b855c3
Tomasz Skupiński <tomasz.skupinski@chimeraprime.com>
2019-09-15 13:30:11 +0200

    Tweak tests configuration and add helpers

---
+++ spec/rails_helper.rb
@@ -0,0 +1,37 @@
+ENV['RAILS_ENV'] ||= 'test'
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require File.expand_path('../../config/environment', __FILE__)
+
+require 'rspec/rails'
+require 'spec_helper'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.fixture_path = 'spec/fixtures/files'
+
+  config.use_transactional_fixtures = true
+
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.include ActiveJob::TestHelper
+  config.include ActionDispatch::TestProcess::FixtureFile
+  config.include ActiveSupport::Testing::TimeHelpers
+  config.include FactoryBot::Syntax::Methods
+  config.include RequestHelpers, type: :request
+
+  def load_json_fixture(name)
+    data = file_fixture("../#{name}.json").read
+    JSON.parse(data).with_indifferent_access
+  end
+end
