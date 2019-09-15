39be15c622d9391658a3129ed5a9dd38cd1f3c5c
Tomasz Skupiński <tomasz.skupinski@chimeraprime.com>
2019-09-15 14:15:55 +0200

    Add Trips API requests spec

---
+++ spec/requests/trips_request_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Trips API' do
+  describe 'POST /api/trips' do
+    let(:path) { '/api/trips' }
+
+    let(:params) { load_json_fixture(:trip) }
+
+    subject! { post path, params: params }
+
+    it 'returns the 201 status code' do
+      expect(response).to have_http_status(201)
+    end
+
+    it 'creates a trip' do
+      expect(Trip.count).to eq(1)
+    end
+
+    it 'returns trip details' do
+      expect(json_response['id']).to be_present
+      expect(json_response['start_address']).to be_present
+      expect(json_response['destination_address']).to be_present
+      expect(json_response['price']).to be_present
+      expect(json_response['date']).to be_present
+    end
+  end
+end
